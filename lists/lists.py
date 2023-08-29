4e70f7acfaf2238381838c202e4249a620c7268b
tod619 <trevorodwyer619@hotmail.com>
2023-08-29 21:30:53 +0100

    Complete lesson Advanced operations in lists.py file

--- lists/lists.py
+++ lists/lists.py
@@ -13,4 +13,33 @@ for item in my_list:
 del my_list[0]
 print(my_list)
 
-# End Lesson List in Python
+# End: Lesson List in Python
+
+# Start: Lesson Advanced Operations
+# Appending item at the end is big O (1)
+my_list.append("New Item added to the end")
+print(my_list)
+
+# Access the end of the list using - 
+# -1 index access the last item in the list
+print(my_list[-1])
+
+# Print a range of items seperate using :
+# Includes the first item but excludes the second item
+print(my_list[0:3])
+
+# Prints the entire list
+print(my_list[:])
+
+# Prints everything up to the last item but does not includ the last itme
+print(my_list[:-1])
+
+# Reverses the list
+print(my_list[::-1])
+
+# Lists can be added together
+list1 = ["List1", 5, 7, 10.9]
+list2 = ["list2", 23, 45, 9.4]
+result = list1 + list2
+print(result)
+# End: Lesson Advanced Operations
