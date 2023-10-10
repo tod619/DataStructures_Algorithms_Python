8832843b2dad69e01ee1a41a057f00e8fa11a33d
tod619 <trevorodwyer619@hotmail.com>
2023-10-10 20:25:49 +0100

    Create func to add items at end of list in ll.py file

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -21,8 +21,26 @@ class Linked_List:
         if self.head is None:
             self.head = new_node
         else:
+            # This is executed when the Linked List is not empty we update the references
             new_node.next_node = self.head
             self.head = new_node
+
+    # Insert at the end of the list
+    def insert_end(self, data):
+        self.num_of_nodes += 1
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            # This is why the runtie is 0(N) linear run time
+            actual_node = self.head
+            while actual_node.next_node is not None:
+                actual_node = actual_node.next_node
+
+            actual_node.next_node = new_node
+
+        
 
     # Return the size of the list 0(1) constant time
     def size_of_list(self):
