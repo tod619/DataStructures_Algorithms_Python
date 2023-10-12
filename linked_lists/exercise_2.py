189dd60e318911568ef46d395e26f0014ba3aa8d
tod619 <trevorodwyer619@hotmail.com>
2023-10-12 20:54:07 +0100

    Add starter code in exercise_2.py file

--- linked_lists/exercise_2.py
+++ linked_lists/exercise_2.py
@@ -1,2 +1,42 @@
 # Reverse a Linked List
 # 12/10/2023
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    # O(N) linear running time complexity
+    def reverse(self):
+        # your algorithm goes here !!!
+        pass
+
+    def insert(self, data):
+
+        self.size = self.size + 1
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+            
+    def get_head(self):
+        return self.head
+
+    def traverse_list(self):
+
+        actual_node = self.head
+
+        while actual_node is not None:
+            print("%d" % actual_node.data)
+            actual_node = actual_node.next_node
