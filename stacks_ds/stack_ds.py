080184b4b21683156930f5cce5b7f301aa727e50
tod619 <trevorodwyer619@hotmail.com>
2023-10-13 20:55:38 +0100

    Create a stack and pop the items of the stack

--- stacks_ds/stack_ds.py
+++ stacks_ds/stack_ds.py
@@ -24,5 +24,23 @@ class Stack:
     # Create is empty function that returns true/false if the stack has an item or not
     def is_empty(self):
         return self.stack == []
+    
+    # stack_size returns the size of the Stack
+    def stack_size(self):
+        return len(self.stack)
 
 
+my_stack = Stack()
+my_stack.push(5)
+my_stack.push(10)
+my_stack.push(15)
+my_stack.push(20)
+my_stack.push(25)
+my_stack.push(30)
+my_stack.push(55)
+
+print(f"The size of my stack is: {my_stack.stack_size()}")
+while my_stack.is_empty() != True:
+    print(my_stack.pop())
+
+print(f"The size of my stack when all the items have been popped off: {my_stack.stack_size()}")
