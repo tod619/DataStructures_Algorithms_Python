66cccc05664935e55bc7bc76765fc83a9a767f8c
tod619 <trevorodwyer619@hotmail.com>
2023-10-13 20:38:59 +0100

    Create push + pop function in stack_ds.py file

--- stacks_ds/stack_ds.py
+++ stacks_ds/stack_ds.py
@@ -7,3 +7,12 @@ class Stack:
     def __init__(self):
         self.stack = []
 
+    # Create push method that inserts items onto the stack Big O(1)
+    def push(self, data):
+        self.stack.append(data)
+
+    # Create pop method that returns the item at the top of the stack the Last In Big O(1)
+    def pop(self):
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data 
