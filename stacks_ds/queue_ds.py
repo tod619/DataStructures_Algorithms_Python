df3741974cbfd1f22f6abbe537e70c147d10b80e
tod619 <trevorodwyer619@hotmail.com>
2023-10-14 20:27:05 +0100

    Use the queue functions with my_queue

--- stacks_ds/queue_ds.py
+++ stacks_ds/queue_ds.py
@@ -24,4 +24,21 @@ class Queue:
     def size_of_queue(self):
         return len(self.queue)
     
-    
+my_queue = Queue()
+my_queue.enqueue(2)
+my_queue.enqueue(4)
+my_queue.enqueue(6)
+my_queue.enqueue(8)
+my_queue.enqueue(10)
+my_queue.enqueue(12)
+my_queue.enqueue(14)
+my_queue.enqueue(16)
+my_queue.enqueue(18)
+
+print(f"The size of my queue is: {my_queue.size_of_queue()}")
+print(f"The first item on my queue is: {my_queue.peek()}")
+
+while my_queue.is_empty() != True:
+    print(my_queue.dequeue())
+
+print(f"The size of my queue when all the itmes have been dequeued is: {my_queue.size_of_queue()}")
