49a65fd313d881202d2b091b433ca6282700c4c7
tod619 <trevorodwyer619@hotmail.com>
2023-10-14 20:15:53 +0100

    Create helper functions for the queue

--- stacks_ds/queue_ds.py
+++ stacks_ds/queue_ds.py
@@ -6,3 +6,22 @@ class Queue:
 
     def __init__(self):
         self.queue = []
+
+    def is_empty(self):
+        return self.queue == []
+    
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        data = self.queue[0]
+        del self.queue[0]
+        return data
+    
+    def peek(self):
+        return self.queue[0]
+    
+    def size_of_queue(self):
+        return len(self.queue)
+    
+    
