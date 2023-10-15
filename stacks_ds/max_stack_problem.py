0805545d3805332314aa3c233bb84ec03fe427fd
tod619 <trevorodwyer619@hotmail.com>
2023-10-15 21:08:58 +0100

    create Stack class + helper function

--- stacks_ds/max_stack_problem.py
+++ stacks_ds/max_stack_problem.py
@@ -1,3 +1,24 @@
 # a Function to return the max item on the stack
 # 15/10/2023
 
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+
+        if self.stack_size < 0:
+            return
+        
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+    
+    def stack_size(self):
+        return len(self.stack)
+    
+    
