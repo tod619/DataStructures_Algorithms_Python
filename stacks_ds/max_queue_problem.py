0d547fe838292ebed35a0c29402f9031d22e5924
tod619 <trevorodwyer619@hotmail.com>
2023-10-16 20:23:35 +0100

    Add queue code ot file

--- stacks_ds/max_queue_problem.py
+++ stacks_ds/max_queue_problem.py
@@ -1,2 +1,26 @@
 # Get the max value from a queue
 # 16/10/2023
+
+# queue is FIFO First In First Out
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+        self.max_queue = []
+
+    def is_empty(self):
+        return self.queue == []
+    
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        data = self.queue[0]
+        del self.queue[0]
+        return data
+    
+    def peek(self):
+        return self.queue[0]
+    
+    def size_of_queue(self):
+        return len(self.queue)
