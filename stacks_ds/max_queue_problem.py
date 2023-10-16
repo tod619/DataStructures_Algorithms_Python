e5efed671219b7a98cccca37d7438a6e15c6e1e2
tod619 <trevorodwyer619@hotmail.com>
2023-10-16 20:36:35 +0100

    Implement a queue with 2 stacks

--- stacks_ds/max_queue_problem.py
+++ stacks_ds/max_queue_problem.py
@@ -1,26 +1,27 @@
-# Get the max value from a queue
+# Implement the Queue data struct with a stack
 # 16/10/2023
 
 # queue is FIFO First In First Out
 class Queue:
 
     def __init__(self):
-        self.queue = []
-        self.max_queue = []
+       # Use 1 stack for enqueueu
+       self.enqueue_stack = []
+       # Use 1 stack for dequeue
+       self.dequeue_stack = []
 
-    def is_empty(self):
-        return self.queue == []
-    
+    # Add an itme to the stack 0(1)
     def enqueue(self, data):
-        self.queue.append(data)
+        self.enqueue_stack.append(data)
 
     def dequeue(self):
-        data = self.queue[0]
-        del self.queue[0]
-        return data
-    
-    def peek(self):
-        return self.queue[0]
-    
-    def size_of_queue(self):
-        return len(self.queue)
+        if len(self.enqueue_stack) == 0 and len(self.dequeue_stack) == 0:
+            raise Exception("Stacks are empty....")
+        
+        if len(self.enqueue_stack_stack) == 0:
+            while len(self.enqueue_stack) != 0:
+                self.dequeue_stack.append(self.enqueue_stack.pop())
+
+        return self.dequeue_stack.pop()
+
+        
