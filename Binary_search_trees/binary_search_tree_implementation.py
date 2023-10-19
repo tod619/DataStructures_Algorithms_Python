8b476edc42b3bff0c2966b3a6dcd95f9b1f8a6fa
tod619 <trevorodwyer619@hotmail.com>
2023-10-19 20:14:48 +0100

    Create Node class for binary search tree in file

--- Binary_search_trees/binary_search_tree_implementation.py
+++ Binary_search_trees/binary_search_tree_implementation.py
@@ -1,2 +1,10 @@
 # An Implementation of a binary search tree
 # 17/10/2023
+
+class Node:
+
+    def __init__(self, data, parent=None):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.parent = parent
