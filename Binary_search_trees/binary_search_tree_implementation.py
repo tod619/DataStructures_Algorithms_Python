6e214a42e746ed03887165143714a2c07046641b
tod619 <trevorodwyer619@hotmail.com>
2023-10-19 20:32:49 +0100

    Create unsert + insert_node functions in bts.py file

--- Binary_search_trees/binary_search_tree_implementation.py
+++ Binary_search_trees/binary_search_tree_implementation.py
@@ -8,3 +8,31 @@ class Node:
         self.left_node = None
         self.right_node = None
         self.parent = parent
+
+
+class BinarySearchtree:
+
+    def __init__(self):
+        self.root = None
+
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # Go to left sub tree
+        if data < node.data:
+            if node.left_node:
+                self.insert_node(data, node.left_node)
+            else:
+                node.left_node = Node(data, node)
+
+        else:
+            # Go to the right sub tree
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node)
